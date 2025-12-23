a83341040b9200c722d0511b6457a245a4134ac5
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-12-23 16:06:37 +0900

    0007

--- src/main/java/com/back/boundedContext/member/app/MemberService.java
+++ src/main/java/com/back/boundedContext/member/app/MemberService.java
@@ -1,7 +1,7 @@
-package com.back.boundedContext.member.service;
+package com.back.boundedContext.member.app;
 
-import com.back.boundedContext.member.entity.Member;
-import com.back.boundedContext.member.repository.MemberRepository;
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.exception.DomainException;
 import org.springframework.stereotype.Service;
 
