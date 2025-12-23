85de1a8f4a6a25d0a2e45da1d184012e079f63f7
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-12-23 11:33:35 +0900

    0001

---
+++ src/main/java/com/back/jpa/entity/BaseIdAndTime.java
@@ -0,0 +1,28 @@
+package com.back.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+/**
+ * 굳이 왜 BaseEntity에서 분리했느냐?
+ * 이후에 id가 없는 도메인이 존재할 수 있기 때문
+ */
+public class BaseIdAndTime {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
