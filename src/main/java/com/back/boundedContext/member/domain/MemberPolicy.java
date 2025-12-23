b3b8a9aece38434c0200d548448c430c4001f4f4
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-12-23 17:50:50 +0900

    0010

---
+++ src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
@@ -0,0 +1,27 @@
+package com.back.boundedContext.member.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+
+@Service
+public class MemberPolicy {
+    private static final int PASSWORD_CHANGE_DAYS = 90;
+
+    public Duration getNeedToChangePasswordPeriod() {
+        return Duration.ofDays(PASSWORD_CHANGE_DAYS);
+    }
+
+    public int getNeedToChangePasswordDays(){
+        return PASSWORD_CHANGE_DAYS;
+    }
+
+    public boolean isNeedToChangePassword(LocalDateTime lastChangeAt) {
+        if (lastChangeAt == null) return true;
+
+        return lastChangeAt.plusDays(PASSWORD_CHANGE_DAYS)
+                .isBefore(LocalDateTime.now());
+    }
+}
