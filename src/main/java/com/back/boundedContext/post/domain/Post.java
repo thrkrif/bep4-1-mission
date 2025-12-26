abebf60dc3903ad5de0cfc8042a33db2e15fdac4
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-12-26 09:16:28 +0900

    0015

--- src/main/java/com/back/boundedContext/post/domain/Post.java
+++ src/main/java/com/back/boundedContext/post/domain/Post.java
@@ -14,6 +14,7 @@ import java.util.List;
 import static jakarta.persistence.CascadeType.PERSIST;
 import static jakarta.persistence.CascadeType.REMOVE;
 @Entity
+@Table(name = "POST_POST")
 @NoArgsConstructor
 @Getter
 public class Post extends BaseIdAndTime {
