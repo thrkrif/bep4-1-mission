d4a0448cc591070c7cc977698f53daa57737aa85
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-12-31 08:42:42 +0900

    0045

--- src/main/java/com/back/boundedContext/payout/out/PayoutCandidateItemRepository.java
+++ src/main/java/com/back/boundedContext/payout/out/PayoutCandidateItemRepository.java
@@ -2,6 +2,10 @@ package com.back.boundedContext.payout.out;
 
 import com.back.boundedContext.payout.domain.PayoutCandidateItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PayoutCandidateItemRepository extends JpaRepository<PayoutCandidateItem, Integer> {
+    List<PayoutCandidateItem> findByPayoutItemIsNullAndPaymentDateBeforeOrderByPayeeAscIdAsc(LocalDateTime paymentDate, Pageable pageable);
 }
